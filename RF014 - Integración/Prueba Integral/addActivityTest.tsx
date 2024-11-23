7eb494bc2299b37436482d21ef602f2631f20854
Adrian Josue Vega Barrantes <100384007+AdrianVega1@users.noreply.github.com>
2024-11-22 23:19:29 -0600

    Update addActivityTest.tsx

--- RF014 - Integración/Prueba Integral/addActivityTest.tsx
+++ RF014 - Integración/Prueba Integral/addActivityTest.tsx
@@ -1,6 +1,8 @@
 import { render, fireEvent, screen } from '@testing-library/react';
-import Page from '../app/newActivity/page'; 
+import Page from '../app/newActivity/page';
 import { handlerAddActivity } from '../controller/actividadController';
+import 'fake-indexeddb/auto';
+
 
 jest.mock('next/navigation', () => ({
     useRouter: () => ({
@@ -16,7 +18,7 @@ jest.mock('next/navigation', () => ({
 }));
 
 jest.mock('../controller/actividadController', () => ({
-    handlerAddActivity: jest.fn(),
+    handlerAddActivity: jest.fn().mockResolvedValue({ success: true }), 
 }));
 
 beforeAll(() => {
@@ -38,10 +40,48 @@ describe('handleSubmit Integration Tests', () => {
 
     it('should add an activity successfully when valid data is provided', () => {
         render(<Page />);
-        
+
         // Simulate user inputs
         fireEvent.change(screen.getByLabelText('Nombre'), {
-            target: { value: 'Actividad 1' },
+            target: { value: 'Nueva Actividad' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Semana de realizacion'), {
+            target: { value: 'Semana 1' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Tipo'), {
+            target: { value: 'Orientadora' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Modalidad'), {
+            target: { value: 'Presencial' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Fecha'), {
+            target: { value: '11/30/2024' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Hora'), {
+            target: { value: '10:12 AM' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Iniciar recordatorio'), {
+            target: { value: '11/25/2024' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Enlace'), {
+            target: { value: 'https://example.com' },
+        });
+
+        fireEvent.change(screen.getByLabelText('Frecuencia de recordatorios'), {
+            target: { value: '1' },
+        });
+
+        // Simulate file upload for "Afiche"
+        const file = new File(['dummy content'], 'afiche.png', { type: 'image/png' });
+        fireEvent.change(screen.getByLabelText('Afiche'), {
+            target: { files: [file] },
         });
 
         // Simulate form submission
@@ -53,7 +93,7 @@ describe('handleSubmit Integration Tests', () => {
 
     it('should not add an activity when invalid data is provided', () => {
         render(<Page />);
-        
+
         // Simulate user inputs with invalid data
         fireEvent.change(screen.getByLabelText('Nombre'), {
             target: { value: '' }, // Empty name
